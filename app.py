f35dbf405555b609439aa108f5c3e8d1abc7830d
akramwasimjmi1994-wq <akramwasimjmi.1994@gmail.com>
2026-03-01 00:39:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,4 +56,9 @@ def pdf_to_images():
 
 if __name__ == "__main__":
     if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
